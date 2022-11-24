b87f219619a97b577a016ab4aa8cca6f744b4f5c
manou141 <manou141@hotmail.com>
2022-11-23 21:01:53 -0500

    2d game carmanua

---
+++ src/cars/Work.java
@@ -0,0 +1,19 @@
+package cars;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+
+public class Work  extends JPanel implements ActionListener{
+
+    private int space;
+    private int speed;
+    private int WIDTH;
+    private int HEIGHT;
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+}
